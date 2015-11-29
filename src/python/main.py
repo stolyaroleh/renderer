547ea3c5f2aa6391389adec4443bcdb65fb62680
Oleh Stolyar <stolyaroleh@users.noreply.github.com>
2015-11-30 01:52:10 +0200

    Added a dark theme

--- src/python/main.py
+++ src/python/main.py
@@ -1,15 +1,20 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from qdarkstyle import load_stylesheet_pyqt5
 from ui.main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     window = QMainWindow()
+
+    if '--dark' in sys.argv:
+        stylesheet = load_stylesheet_pyqt5()
+        window.setStyleSheet(stylesheet)
+
     main_ui = MainWindow(window)
     main_ui.setupUi(window)
     main_ui.initialize()
-    window.show()
 
+    window.show()
     sys.exit(app.exec_())
