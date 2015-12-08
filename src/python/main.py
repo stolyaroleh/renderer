fbeff847337f28a61d9caf30cc0ab801383fb314
Oleh Stolyar <stolyar.oleh@gmail.com>
2015-12-09 00:33:22 +0200

    Implement console widget and redirect STDOUT to it

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,21 @@
+import os
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from qdarkstyle import load_stylesheet_pyqt5
 from ui.main_window import MainWindow
+
+
+class FDWrapper:
+    def __init__(self, fd):
+        self.fd = fd
+
+    def write(self, message):
+        os.write(self.fd, message.encode())
+
+    def flush(self):
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -12,9 +25,17 @@ if __name__ == '__main__':
         stylesheet = load_stylesheet_pyqt5()
         window.setStyleSheet(stylesheet)
 
-    main_ui = MainWindow(window)
+    # capture file descriptors, responsible for stderr and stdout and pass them to console for logging
+    stdout_descriptor = sys.stdout.fileno()
+    new_stdout, in_write = os.pipe()
+    os.dup2(in_write, stdout_descriptor)
+    sys.stdout = FDWrapper(in_write)
+
+    main_ui = MainWindow(window, new_stdout)
     main_ui.setupUi(window)
     main_ui.initialize()
+
+    print('Initializing...')
 
     window.show()
     sys.exit(app.exec_())
--- src/python/ui/main_window.py
+++ src/python/ui/main_window.py
@@ -1,12 +1,29 @@
 from ui_generated.main_window_ui import Ui_MainWindow
+from ui.console_widget import ConsoleWidget
 
 
 class MainWindow(Ui_MainWindow):
-    def __init__(self, parent):
+    def __init__(self, parent, out_pipe):
         self.parent = parent
+        self.console = ConsoleWidget(out_pipe)
 
     def initialize(self):
-        self.actionQuit.triggered.connect(self.close)
+        self.console.setupUi(self.console_dock_contents)
+        self.console.initialize()
+
+        self.action_console.triggered.connect(self.toggle_log)
+        self.action_render_view.triggered.connect(self.toggle_render_view)
+        self.action_scene_view.triggered.connect(self.toggle_scene_view)
+        self.action_quit.triggered.connect(self.close)
+
+    def toggle_log(self):
+        self.console_dock.setVisible(self.action_console.isChecked())
+
+    def toggle_render_view(self):
+        self.render_view.setVisible(self.action_render_view.isChecked())
+
+    def toggle_scene_view(self):
+        self.scene_view.setVisible(self.action_scene_view.isChecked())
 
     def close(self):
         self.parent.close()
